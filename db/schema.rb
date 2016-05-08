cd759029f6960287a3e60436ae7bd86675d631ca
hsaadut <hsaadut@ggc.edu>
2016-05-08 19:47:06 +0000

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509165632) do
+ActiveRecord::Schema.define(version: 20160508165632) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
