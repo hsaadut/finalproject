f370a8cb1ef69db51ef6f647014530cdaba34f79
hsaadut <hsaadut@ggc.edu>
2016-05-08 03:44:34 +0000

    Project

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Book.create(title: "Harry Potter", author: "J.K. Rowling", comment: "Mysterious book and the only one I can think of")
+Book.create(title: "Ruby on Rails", author: "Michael Hartl", comment: "I learned something new")
+Book.create(title: "Hamlet", author: "William Shakespeare", comment: "Read this book in my ENGL 2110")
+
+
